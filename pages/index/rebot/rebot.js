128f2aa7d451fc0e3479820760c2e3f6a5bc6807
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-03-02 17:53:40 +0800

    优化聊天界面

--- pages/index/rebot/rebot.js
+++ pages/index/rebot/rebot.js
@@ -1,4 +1,4 @@
-// pages/index/rebot/rebot.js
+
 Page({
 
   /**
@@ -6,14 +6,23 @@ Page({
    */
   data: {
     message: '',
-    content:[]
+    content: [],
+    usermessage: '',
+    avatar: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    wx.getUserInfo({
+      success: function (userInfo) {
+        that.setData({
+          avatar: userInfo.userInfo.avatarUrl
+        })
+      }
+    });
   },
 
   /**
@@ -53,31 +62,45 @@ Page({
    * 发送消息按钮
    */
   sendMessage: function (event) {
-    var that=this
+    var that = this
     var mess = that.data.message;
     if (mess == null || mess === '') {
       wx.showToast({
         title: '消息不能为空'
       })
     } else {
-      var appkey = "147c3451003239dcb1ac1dda32660d4e";
-      var datas=that.data.content
-      datas.push({"rebot":false,"text":mess})
+      var appkey = "57992";
+      var sign = "81b5351e889649efb514ddf707a869c4";
+      var datas = that.data.content
+      var avatar = that.data.avatar
+      datas.push({ "rebot": false, "text": mess, "avatar": avatar })
       that.setData({
-        content:datas
+        content: datas,
+        usermessage: ''
       })
-      var url = "https://v.juhe.cn/robot/index?key=" + appkey + "&info=" + mess;
+       var i=400
+      var url = "https://route.showapi.com/60-27?showapi_appid=" + appkey + "&info=" + mess + "&showapi_sign=" + sign;
       wx.request({
         url: url,
-        success:function(e){
-          console.log(e)
+        success: function (result) {
+          console.log()
+          if (result.data.showapi_res_code == 0) {
+            datas.push({ "rebot": true, "text": result.data.showapi_res_body.text })
+            that.setData({
+              content: datas
+            })
+            wx.pageScrollTo({
+              scrollTop: i+10,
+              duration: 300
+            })
+          } else {
+            datas.push({ "rebot": true, "text": "智能机器人罢工了" })
+            that.setData({
+              content: datas
+            })
+          }
         }
       })
-      datas.push({ "rebot": true, "text": "我是智能机器人" })
-      that.setData({
-        content: datas
-      })
-
     }
   },
   /**
@@ -88,5 +111,8 @@ Page({
     that.setData({
       message: e.detail.value
     })
+  },
+  backbottom:function(e){
+   console.log(e)
   }
 })
